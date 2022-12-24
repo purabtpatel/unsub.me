03d2825bb0b45d8861998962dbe07e6331ea4c22
purabtpatel <purabtpatel24@gmail.com>
2022-12-24 12:08:23 -0500

    basic authentication

--- web/src/layouts/NavigationLayout/NavigationLayout.js
+++ web/src/layouts/NavigationLayout/NavigationLayout.js
@@ -1,11 +1,26 @@
 import { Link, routes } from '@redwoodjs/router'
+import { useAuth } from '@redwoodjs/auth'
 
 const NavigationLayout = ({ children }) => {
+  const { isAuthenticated, currentUser, logOut } = useAuth()
   return <>
     <header>
-      <h1>
-        <Link to={routes.home()}>UnSub.me</Link>
-      </h1>
+      <div className="flex-between">
+
+        <h1>
+          <Link to={routes.home()}>UnSub.me</Link>
+        </h1>
+        {isAuthenticated ? (
+          <div>
+            <span>Logged in as {currentUser.email}</span>
+            <button onClick={logOut}>Log out</button>
+          </div>
+        ) : (
+          <Link to={routes.login()}>Log in</Link>
+        )}
+
+
+      </div>
       <nav>
         <ul>
           <li>
